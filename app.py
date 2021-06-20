c036b81a75a33ab6484c979b0ffddcef48c45bc3
Adriano Sousa <adrianocs02@gmail.com>
2021-06-20 16:42:52 -0300

    add logs

--- app.py
+++ app.py
@@ -33,14 +33,17 @@ async def readfile(image: UploadFile = Form(...)):
     # Upload the file
     file_id = str(uuid.uuid4())
     s3 = boto3.client('s3', region_name = 'us-east-1')
+    logger.info('connected to s3')
     try:
-        s3.upload_fileobj(image.file, BUCKET_NAME, file_id)        
+        s3.upload_fileobj(image.file, BUCKET_NAME, file_id) 
+        logger.info('uploaded')       
     except ClientError as e:
         logger.error(e)
         raise HTTPException(status_code=500, detail="Can't process image")
     
     # Reading text
     textract = boto3.client('textract', region_name = 'us-east-1')
+    logger.info('connected to textract')
     response = textract.detect_document_text(
     Document={
         'S3Object': {
@@ -52,6 +55,7 @@ async def readfile(image: UploadFile = Form(...)):
     for item in response["Blocks"]:
         if item["BlockType"] == "LINE":
             text += (item["Text"] + '\n')
+    logger.info(text)
     return {"text": text}
 
 handler = Mangum(app=app)
