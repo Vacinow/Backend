b27c4fc436247d4d4d3ae9afe9c3c6c30e007653
Adriano Sousa <adrianocs02@gmail.com>
2021-06-24 11:23:48 -0300

    moving to okteto

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM amazon/aws-lambda-python:3.8
+FROM python:3.8
 
-RUN yum update -y
+WORKDIR /usr/src/app
 
 COPY requirements.txt ./
-COPY *.py ./
+RUN pip install --no-cache-dir -r requirements.txt
 
-RUN pip install -r requirements.txt
+COPY . .
 
-CMD ["app.handler"]
+EXPOSE 80
+
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "80"]
--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from fastapi import FastAPI, HTTPException, UploadFile, Form
-from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
-from mangum import Mangum
-import uuid
 
 import boto3
 from botocore.exceptions import ClientError
 
 import logging
 
-BUCKET_NAME = 'vacinowbucket'
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+app = FastAPI()
 
-middleware = [Middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])]
-app = FastAPI(middleware=middleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -24,22 +25,9 @@ async def root():
 
 @app.post("/readfile/")
 async def readfile(image: UploadFile = Form(...)):
-    # # Upload the file
-    # file_format = image.filename.split('.')[-1]
-    # file_id = str(uuid.uuid4())+'.'+file_format
-    # s3 = boto3.client('s3', region_name = 'us-east-1')
-    # logger.info('connected to s3')
-    # try:
-    #     s3.upload_fileobj(image.file, BUCKET_NAME, file_id) 
-    #     logger.info('uploaded')       
-    # except ClientError as e:
-    #     logger.error(e)
-    #     raise HTTPException(status_code=500, detail="Can't process image")
-    
-    # Reading text
+
     textract = boto3.client('textract', region_name = 'us-east-1')
     logger.info('connected to textract')
-    #logger.info(file_id)
     response = textract.detect_document_text(
     Document={
         'Bytes': image.file.read()
@@ -50,5 +38,3 @@ async def readfile(image: UploadFile = Form(...)):
             text += (item["Text"] + '\n')
     logger.info(text)
     return {"text": text}
-
-handler = Mangum(app=app)
